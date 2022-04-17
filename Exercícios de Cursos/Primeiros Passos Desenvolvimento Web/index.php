a42eae4fabcb5c1395ad3a69b5e25c0bb9c0acb1
Carlos Eduardo Tavares <carlos_ed.tavares@hotmail.com>
2022-04-17 17:30:48 -0300

    Add Exercicios Primeiros passos desenvolvimento WEB e HTML & CSS

---
+++ Exercícios de Cursos/Primeiros Passos Desenvolvimento Web/index.php
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+	
+	    <tittle>Meu primeiro site em PHP! yeaaah ! <br /><br /></tittle>
+
+	</head>
+	
+	<body> 
+	    
+		
+		
+	    <?php
+            for ( $i = 0 ; $i <= 10 ; $i++ ) {
+				print( "Linha número " . $i . "<br />");
+			}
+		?>
+	
+	</body>
+
+</html>
---
+++ Exercícios de Cursos/Primeiros Passos Desenvolvimento Web/pagina.php
@@ -0,0 +1,75 @@
+<html>
+
+    <head>
+	
+	    <tittle><h3>Exercício Final - Accordion</h3></tittle>
+        
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>jQuery UI Accordion - Default functionality</title>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+        <script>
+        $( function() {
+            $( "#accordion" ).accordion();
+        } );
+        </script>
+		
+	</head>
+	
+	<body> 
+	
+    <div id="accordion">
+        <h3>Texto</h3>
+        <div>
+            <p>
+            Amar lê habilidade - Maleabilidade <br /><br />
+            Sinuosa escuridão que toma forma e segue teu passos <br />
+            Enfatizando uma nota em cada arpejo versificado <br />
+            No oscilar inconsciente, sentimentos impostos e revelados <br /> 
+            Um sabor que acompanha uma vida <br />
+            ... com lembranças e carências... o devaneio da ação <br />
+            Algo capaz de mover corpos a sua vontade <br />
+            Névoa espessa que toma teu bailar, tato e audição <br />
+            Saboreando horas segundos percorridos em meio a sensibilidade <br />
+            Elísio para os que ignoram o tempo dentro de sua longevidade <br /><br />
+            Eternamente Sombrio?... <br />
+            by Carlos Eduardo <br />
+            27/03/2022 14:50
+            </p>
+        </div>
+        <h3>Strogonoff de Frango</h3>
+        <div>
+            <p>
+            <h4>Modo de Preparo<br /></h4>
+			1. Em uma panela, misture o frango, o alho, a maionese, o sal e a pimenta.<br />
+            2. Em uma frigideira grande, derreta a manteiga e doure a cebola.<br />
+            3. Junte o frango temperado até que esteja dourado.<br />
+            4. Adicione os cogumelos, o ketchup e a mostarda.<br />
+            5. Incorpore o creme de leite e retire do fogo antes de ferver.<br />
+            6. Sirva com arroz branco e batata palha.<br />
+            </p>
+            <ul>
+                <li>3 cortados em cubos</li>
+                <li>1 dente de alho picado</li>
+                <li>1 cebola picada</li>
+				<li>2 colheres (sopa) de maionese</li>
+				<li>1 colher de manteiga</li>
+				<li>1/2 copo de ketchup</li>
+				<li>1/3 copo de mostarda</li>
+				<li>1 copo de cogumelos</li>
+				<li>1 copo de creme de leite</li>
+				<li>batata palha a gosto</li>
+            </ul>
+        </div>
+		<h3>...</h3>
+		<div>    
+			<p>
+			<h3>Fim!</h3>
+			</p>
+		</div>	
+    </body>
+
+</html>
